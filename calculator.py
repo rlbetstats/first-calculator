2ebf7d8053087a9ef60b67e98c5d9b09f2acca73
Liam R <43114303+rlbetstats@users.noreply.github.com>
2018-09-09 20:17:24 +0100

    Second update with new 'add' code

--- calculator.py
+++ calculator.py
@@ -3,6 +3,7 @@
 
 while True:
     print("Options: ")
+    print("---------")
     print("Enter 'add' to add two numbers together")
     print("Enter 'subtract' to take two numbers away from each other")
     print("Enter 'multiply' to multiply two numbers together")
@@ -13,7 +14,10 @@ while True:
     if user_input == "quit":
         break
     elif user_input == "add":
-        print("To be written (add)")
+        first_add = input("Please enter the first number: ")
+        second_add = input("Please enter the second number to add: ")
+        print("The answer is " + str(int(first_add) + int(second_add)))
+        print("")
     elif user_input == "subtract":
         print("To be written (subtract)")
     elif user_input == "multiply":
